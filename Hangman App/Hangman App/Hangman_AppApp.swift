0eb301d02c2b22c702d08a840ed7efb392318c48
Quincy Poynter <quincypoynter@berkeley.edu>
2023-04-19 17:08:57 -0700

    add project file

---
+++ Hangman App/Hangman App/Hangman_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hangman_AppApp.swift
+//  Hangman App
+//
+//  Created by Quincy Poynter on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Hangman_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
